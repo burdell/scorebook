5e2ae6faa04a0805f0432c980b8c10cf1561d2ba
Nathan Griffin <nathanjgriffin@gmail.com>
2020-05-11 22:04:48 -0400

    initial commit

---
+++ src/components/Nav.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let segment;
+</script>
+
+<style>
+  nav {
+    border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+    font-weight: 300;
+    padding: 0 1em;
+    display: flex;
+    align-items: center;
+  }
+
+  [aria-current] {
+    position: relative;
+    display: inline-block;
+  }
+
+  a {
+    text-decoration: none;
+    display: block;
+  }
+
+  .site-title {
+    font-size: 1.2rem;
+    padding: 0.5rem 0;
+    font-weight: 300;
+  }
+</style>
+
+<nav>
+  <a aria-current={segment === undefined ? 'page' : undefined} href=".">
+    <div class="site-title">scorekeeper</div>
+  </a>
+</nav>
---
+++ src/routes/game/[gameId].svelte
@@ -0,0 +1,19 @@
+<script context="module">
+  export async function preload(page, session) {
+    const { gameId } = page.params;
+
+    console.log("fucl");
+    const res = await this.fetch(`http://localhost:4000/game/1`);
+    const game = await res.json();
+
+    return { game };
+  }
+</script>
+
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
